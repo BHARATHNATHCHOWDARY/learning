02d1e83dd204a4ec876ed9dc0550d3d209afa37b
BHARATHNATHCHOWDARY <bharathnath.work@gmail.com>
2025-07-19 10:54:15 +0530

    files

--- desk.py
+++ desk.py
@@ -1,1 +1,14 @@
-from turtle import Turtle
+from turtle import Turtle,Screen
+
+turtle=Turtle()
+turtle.shape("square")
+
+screen=Screen()
+screen.listen()
+screen.onkey()
+screen.exitonclick()
+
+def Up():
+    turtle.setheading(90)
+def Down():
+    turtle.setheading
