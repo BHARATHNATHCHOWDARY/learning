134aaa86357a3abfa0a8f4e2d1faa4b6b193db35
BHARATHNATHCHOWDARY <bharathnath.work@gmail.com>
2025-07-19 12:00:22 +0530

    m

--- desk.py
+++ desk.py
@@ -1,18 +1,46 @@
 from turtle import Turtle,Screen
+import time
 
 turtle=Turtle()
 turtle.shape("square")
+turtle.color("white")
+turtle.penup()
+
 
 screen=Screen()
-screen.listen()
-screen.onkey()
-screen.exitonclick()
+screen.tracer(0)
+screen.bgcolor("black")
+
+
 
 def Up():
     turtle.setheading(90)
+    turtle.forward(20)
 def Down():
     turtle.setheading(270)
+    turtle.forward(20)
 def Left():
     turtle.setheading(180)
+    turtle.forward(20)
 def Right():
     turtle.setheading(0)
+    turtle.forward(20)
+
+
+
+screen.listen()
+screen.onkey(Up,"Up")
+screen.onkey(Down,"Down")
+screen.onkey(Left,"Left")
+screen.onkey(Right,"Right")
+
+
+a=10
+while a==10:
+    screen.update()
+    time.sleep(0.1)
+    turtle.forward(20)
+
+screen.exitonclick()
+
+
